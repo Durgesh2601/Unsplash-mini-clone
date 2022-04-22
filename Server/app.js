ad34f5412c76b90775fa2d311b1f0be653353f12
Durgesh Kumar Singh <93372022+Durgesh2601@users.noreply.github.com>
2022-04-23 02:26:03 +0530

    showed the data from backend

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const postController = require("./Controllers/post.controller");
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/", postController);
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -6,9 +6,28 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import Masonry from '@mui/lab/Masonry';
+import React, { useState, useEffect } from 'react';
+
 
 export const Homepage = () => {
-    
+   const [posts, setPosts] = useState([]);
+   useEffect(() => {
+    getPosts();
+   },[])
+   const getPosts = () => {
+       fetch("http://localhost:3000/").then((d) => d.json()).then((res) => {
+           setPosts(res);
+       });
+   }
+//    const mouseEnter = (e) => {
+//      e.target.style.visibility="visible";
+//      console.log(e)
+//    }
+//    const onMouseLeave = (e) => {
+//     e.target.style.visibility="hidden";
+//  }
+   
     return (<>
     <Box sx={{ flexGrow: 1 }} >
       <AppBar position="static" color="transparent">
@@ -24,5 +43,28 @@ export const Homepage = () => {
         </Toolbar>
       </AppBar>
     </Box>
+    <div className="conatiner">
+    <Box sx={{ width: 1300, minHeight: 829 }}>
+      <Masonry columns={3} spacing={4}>
+        {posts.map((e, index) => (
+          <div key={e._id} className="postDiv">
+            <img
+              src={`${e.pic}?w=162&auto=format`}
+              srcSet={`${e.pic}?w=162&auto=format&dpr=2 2x`}
+              alt={e.label}
+              loading="lazy"
+              style={{
+                  borderBottomLeftRadius: 4,
+                  borderBottomRightRadius: 4,
+                  display: 'block',
+                  width: '100%',
+                }}
+                />
+            {/* <Typography variant="p" component="div" align="center" style={{backgroundColor:"black", color:"white"}} onMouseEnter={mouseEnter} onMouseLeave={onMouseLeave}>{e.label}</Typography> */}
+          </div>
+        ))}
+      </Masonry>
+    </Box>
+    </div>
     </>)
 }
